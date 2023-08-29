3b44c1b345783f00eadbd039a5a11cd762178875
Sagar Dutta <107404861+SagarDuttaSays@users.noreply.github.com>
2023-08-29 20:57:59 +0530

    Update readAFile.js

--- readAFile.js
+++ readAFile.js
@@ -1,1 +1,19 @@
+const fs = require('fs')
+const { stringify } = require('querystring')
 
+//synchronous method
+console.log("first line")
+const file = fs.readFileSync('text.txt').toString()
+console.log(file)
+console.log("last line")
+
+//asynchronous method
+console.log("first line")
+f = (err, data)=>{
+    if(err)
+        console.log(err)
+    else 
+        console.log((data).toString())
+}
+fs.readFile('text.txt', f)
+console.log("last line")
